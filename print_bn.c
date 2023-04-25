501dc2bdbdad4bff70511d9911d3fd94b4e04da4
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-25 10:53:39 -0700

    Hot fix

--- print_bn.c
+++ print_bn.c
@@ -10,15 +10,26 @@
 
 int print_bn(int buffer_counter, char *buffer, va_list args)
 {
-	unsigned int number = va_arg(args, unsigned int);
-	unsigned int cup = number;
+	int num = va_arg(args, int);
+	unsigned int number, cup;
 	int binary = 1;
 
+	if (num < 0)
+	{
+		print_buffer((char *)'1', 1);
+		number = (unsigned int)(-1 * num);
+	}
+	else
+		number = num;
+
+
+	cup = number;
 	while (cup > 1)
 	{
 		binary = binary * 2;
 		cup = cup / 2;
 	}
+
 	cup = number;
 	while (binary > 0)
 	{
